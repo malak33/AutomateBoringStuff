fede2fb255d9535c493811f6c1ba506e030067ee
Michael Nobile <mnobile@Michaels-MacBook-Pro.local>
2015-07-20 21:42:40 -0400

    adding password locker program, etc

---
+++ catnapping.py
@@ -0,0 +1,8 @@
+# catnapping.py
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
---
+++ pw.py
@@ -0,0 +1,19 @@
+#! python3
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email': 'F7minBadPassword12345',
+             'blog': 'VmalBlogBadPasswordabcd',
+             'luggage': '987123'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]   # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
