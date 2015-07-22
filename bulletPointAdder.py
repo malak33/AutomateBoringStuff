ea3373aaa70fbd1f954dae5f193f0d93453c51c7
Michael Nobile <nobility33@gmail.com>
2015-07-21 21:26:03 -0400

    addin bulletPintAdder.py

---
+++ bulletPointAdder.py
@@ -0,0 +1,9 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line
+
+import pyperclip
+text = pyperclip.paste()
+# TODO: separate lines and add starts.
+
+pyperclip.copy(text)
