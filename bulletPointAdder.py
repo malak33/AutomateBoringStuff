bef254bd3c2801dee8016d401f070bd0fc9fb9ab
Michael Nobile <nobility33@gmail.com>
2015-07-22 19:39:29 -0400

    adding 2nd try att bulletPointAdder.py

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -6,4 +6,8 @@ import pyperclip
 text = pyperclip.paste()
 # TODO: separate lines and add starts.
 
+lines = text.split('\n')
+for i in range(len(lines)):     # loop through all indexes for "lines" list
+    lines[i] = '* ' + lines[i]  # add star to each string in "lines" list
+text = '\n'.join(lines)
 pyperclip.copy(text)
