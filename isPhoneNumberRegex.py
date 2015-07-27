3b7b6e95d4348dbd5657c6d6fd50deadd6f31b7a
Michael Nobile <nobility33@gmail.com>
2015-07-26 21:01:07 -0400

    regex for isPhoneNumber

---
+++ isPhoneNumberRegex.py
@@ -0,0 +1,7 @@
+#! python3
+# isPhoneNumberRegex.py  - regex to figure out if string of text is a phone number
+import re
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('Phone number found: ' + mo.group())
