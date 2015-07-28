680a8fa140a4430ef0908dafb0c0258852fe76a8
Michael Nobile <nobility33@gmail.com>
2015-07-27 20:25:01 -0400

    first draft of randomQuizGenerator.py

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -5,7 +5,7 @@
 import random
 
 # The quiz data. Keys are states and values are their capitals.as
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizone': 'Phoenix', 'Arkansas': 'Little Rock', 'California':
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California':
 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia':
 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
