0c440a297e4b6f5dca0ae5235dcf9244e294d6d2
Michael Nobile <nobility33@gmail.com>
2015-08-04 21:16:27 -0400

    First draft of renameDates.py

---
+++ readzip.py
@@ -0,0 +1,5 @@
+#! python3
+# readzip.py - Saves and loads pieces of text to the clipboard.
+# Usage: py.exe mcb.pyw save <keyword. - Saves clipboard to keyword.
+#        py.exe mcb.pyw ,keyword. - Loads keyword to clipboard.
+#         py.exe mcb.pyw list - Loads all keywards to clipboard.
