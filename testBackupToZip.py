8a40e3e418cde81a4db089d526931fb8133b6bae
Michael Nobile <nobility33@gmail.com>
2015-08-06 20:43:11 -0400

    Final draft of backupToZip.py

--- testBackupToZip.py
+++ testBackupToZip.py
@@ -1,15 +1,16 @@
 #! python3
-# backupToZip.py - Copies an entire folder and its contents into a ZIP file whose filename increments.
+   # backupToZip.py - Copies an entire folder and its contents into
+   # a ZIP file whose filename increments.
 
 import zipfile, os
-
 
 def backupToZip(folder):
     # Backup the entire contents of "folder" into a ZIP file.
 
-    folder = os.path.abspath(folder)  # make sure folder is absolute
+    folder = os.path.abspath(folder) # make sure folder is absolute
 
-    # Figure out the filename this code should use based on what files already exist
+    # Figure out the filename this code should use based on
+    # what files already exist.
     number = 1
     while True:
         zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
@@ -21,7 +22,7 @@ def backupToZip(folder):
     print('Creating %s...' % (zipFilename))
     backupZip = zipfile.ZipFile(zipFilename, 'w')
 
-    # TODO: Walk the entire folder tree adn compress the files in each folder.
+# Walk the entire folder tree and compress the files in each folder.
     for foldername, subfolders, filenames in os.walk(folder):
         print('Adding files in %s...' % (foldername))
         # Add the current folder to the ZIP file.
@@ -30,10 +31,8 @@ def backupToZip(folder):
         for filename in filenames:
             newBase = os.path.basename(folder) + '_'
             if filename.startswith(newBase) and filename.endswith('.zip'):
-                continue  # don't backup the backup ZIP files
+                continue   # don't backup the backup ZIP files
             backupZip.write(os.path.join(foldername, filename))
     backupZip.close()
     print('Done')
-
-
-backupToZip('/Users/mnobile/Downloads')
+backupToZip('/Users/mnobile')
