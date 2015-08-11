5540a091defd1358858cc26fd9c81591585daedc
Michael Nobile <nobility33@gmail.com>
2015-08-10 20:28:57 -0400

    Final draft of loggingTracebackToFile.py

---
+++ loggingTracebackToFile.py
@@ -0,0 +1,13 @@
+#! python3
+# loggingTracebackToFile.py - shows how to log an error to a file to debug later
+
+import traceback
+
+try:
+    raise Exception('This is the error message.')
+except:
+    errorFile = open('errorInfo.txt', 'w')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('The traceback info was written to errorInfo.txt.')
+
