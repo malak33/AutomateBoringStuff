4e03b19b4a066788ab714383be7669aaf0acd302
Michael Nobile <nobility33@gmail.com>
2015-08-13 20:55:58 -0400

    Final draft of downloadWebPage.py

--- downloadWebPage.py
+++ downloadWebPage.py
@@ -1,8 +1,9 @@
 #! python3
 # downloadWebPage.py - download a web page with the requests.get() function.
 import requests
-res=requests.get('https://automatetheboringstuff.com/chapter10/')
+res=requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+res.raise_for_status()
 type(res)
-res.status_code == requests.codes.ok
+#res.status_code == requests.codes.ok
 len(res.text)
 print(res.text[:250])
