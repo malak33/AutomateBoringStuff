8b00bb2cdef7ab55f9f3e9dc596c78d1272c37c3
Michael Nobile <nobility33@gmail.com>
2015-08-13 21:04:19 -0400

    Final draft of downloadWebPageToFile.py

---
+++ downloadWebPageToFile.py
@@ -0,0 +1,10 @@
+#! python3
+# downloadWebPageToFile.py - this will download the webpage to a file
+
+import requests
+res = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
