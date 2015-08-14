d1fc96ef7b1019c594cdcbf88e0b2dab7b67ae51
Michael Nobile <nobility33@gmail.com>
2015-08-13 20:06:36 -0400

    First draft of mapIt.py

---
+++ buggyAddingProgram.py
@@ -0,0 +1,11 @@
+#! python3
+# - buggyAddingProgram.py - as the title suggest, it is infact a buggy adding program
+
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number to add:')
+second = input()
+print('Enter the third number to add:')
+third = input()
+print('The sum is ' + first + second + third)
+
