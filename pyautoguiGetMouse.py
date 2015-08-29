2bd4cbc7efab2c02cbd2b7a9c1048506bbe9e433
Michael Nobile <nobility33@gmail.com>
2015-08-29 18:03:36 -0400

    added some examples

---
+++ pyautoguiGetMouse.py
@@ -0,0 +1,5 @@
+#! python3
+# pyautoguiGetMouse.py - an example on how to get the screen size
+#
+#  get the mouse position
+print(pyautogui.position())
--- pyautoguiGetScreenSize.py
+++ pyautoguiGetScreenSize.py
@@ -3,3 +3,22 @@
 
 import pyautogui
 print(pyautogui.size())
+
+
+# demo of mouse moving in a rectangle
+for i in range(5):
+    pyautogui.moveTo(100, 100, duration=.25)
+    pyautogui.moveTo(200, 100, duration=.25)
+    pyautogui.moveTo(200, 200, duration=.25)
+    pyautogui.moveTo(100, 200, duration=.25)
+
+
+# demo of mouse moving in a rectange from where the mouse is at currently
+for i in range(5):
+    pyautogui.moveRel(100, 0, duration=.25)
+    pyautogui.moveRel(0, 100, duration=.25)
+    pyautogui.moveRel(-100, 0, duration=.25)
+    pyautogui.moveRel(0, -100, duration=.25)
+
+# get the mouse position
+pyautogui.position()
