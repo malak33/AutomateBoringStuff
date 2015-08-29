4fbe7ce63c8f8be820d6117a7438b60d6d69de87
Michael Nobile <nobility33@gmail.com>
2015-08-29 19:51:14 -0400

    example functions and methods for the pyautogui module

--- pyautoguiGetScreenSize.py
+++ pyautoguiGetScreenSize.py
@@ -2,9 +2,12 @@
 # pyautoguiGetScreenSize.py - an example on how to get the screen size
 
 import pyautogui
+import time
+# have the program sleep for 3 seconds so that i can bring up the text editor
+time.sleep(3)
+
 print(pyautogui.size())
 
-"""
 # demo of mouse moving in a rectangle
 for i in range(5):
     pyautogui.moveTo(100, 100, duration=.25)
@@ -19,12 +22,23 @@ for i in range(5):
     pyautogui.moveRel(0, 100, duration=.25)
     pyautogui.moveRel(-100, 0, duration=.25)
     pyautogui.moveRel(0, -100, duration=.25)
-"""
+
 # get the mouse position
 pyautogui.position()
 
 
 # how to send a left click
 pyautogui.click(10,5)
-# there is also doubleClick, mouseUp, mouseDown, rightClick methods
+# there is also doubleClick, mouseUp, mouseDown, rightClick, and .scroll methods
 
+# how to send keyboard interactions via pyautogui
+pyautogui.click(50,50, duration=.25); pyautogui.typewrite('Hello World', .25)
+
+# these are the different keys that can be pressed
+print(pyautogui.KEYBOARD_KEYS)
+
+# to type a "$" this is how it could be done
+pyautogui.keyDown('shift'); pyautogui.press('4'); pyautogui.keyUp('shift')
+
+# this presses a hotkey
+pyautogui.hotkey('ctrl', 'c')
